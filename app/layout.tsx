187f3bc70b7f14d441e7fcecd089a93c78355117
Yash <yashh.p1801@gmail.com>
2024-05-14 16:16:55 +0530

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { NextAuthProvider } from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextAuthProvider>
+          <div className="lg:max-w-[900px] lg:px-16 mx-auto py-8 shadow-xl min-h-screen flex flex-col px-8">
+            <Navbar />
+            <div className="flex-auto">{children}</div>
+            <Footer />
+          </div>
+        </NextAuthProvider>
+      </body>
     </html>
   );
 }
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import SignInBtns from "@/components/SignInButtons";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function SignIn() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
+  return <SignInBtns />;
+}
